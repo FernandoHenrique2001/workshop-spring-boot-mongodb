c7572b2f4def26ca1724cdb06947dc491ecea635
FernandoHenrique2001 <fernandohenrique.gp@gmail.com>
2021-05-15 18:49:54 -0300

    Acrescentando comentarios aos posts

---
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.example.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.workshopmongo.domain.Post;
+import com.example.workshopmongo.repository.PostRepository;
+import com.example.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
